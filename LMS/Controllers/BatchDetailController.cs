719ddc10590d32c949f4297877df7bd5ae9795b1
thamas161819 <thamas9824@gmail.com>
2023-05-09 12:10:16 +0530

    hi

--- LMS/Controllers/BatchDetailController.cs
+++ LMS/Controllers/BatchDetailController.cs
@@ -29,6 +29,127 @@ namespace LMS.Controllers
             _batchDetailService = batchDetailService;
         }
 
+        //[HttpPost]
+        //public async Task<ActionResult> CreateBatchDetail([FromForm] BDWithChapter batch)
+        //{
+        //    try
+        //    {
+        //        using (var package = new ExcelPackage(batch.File.OpenReadStream()))
+        //        {
+        //            var worksheet = package.Workbook.Worksheets[0]; // assuming the data is on the first worksheet
+        //            var rowCount = worksheet.Dimension.Rows;
+
+        //            for (int i = 2; i <= rowCount; i++) // assuming the first row contains headers
+        //            {
+        //                var chapterCode = worksheet.Cells[i, 1].Value?.ToString();
+        //                var chapterName = worksheet.Cells[i, 2].Value?.ToString();
+        //                var chapterDescription = worksheet.Cells[i, 3].Value?.ToString();
+        //                var expectedDateString = worksheet.Cells[i, 4].Value?.ToString();
+        //                var expectedDate = DateTime.ParseExact(expectedDateString, "dd-MM-yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+
+        //               // var expectedDate = DateTime.ParseExact(expectedDateString, "dd-MM-yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+
+        //                // create a new ChapterBinding object
+        //                var chapter = new ChatperBinding
+        //                {
+        //                    ChapterCode = chapterCode,
+        //                    ChapterName = chapterName,
+        //                    ChapterDescription = chapterDescription,
+        //                    ExpectedDate = expectedDate.Date
+        //                };
+
+        //                // pass the chapter details along with the batch details to your service or database for processing
+        //                await _batchDetailService.CreateBatchDetail(batch, chapter);
+        //            }
+        //        }
+
+        //        // return a success response
+        //        return Ok();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // return an error response
+        //        Log.Error(ex, "Error creating batch details.");
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
+
+
+        //[HttpPost]
+        //public async Task<ActionResult> CreateBatchDetail([FromForm] BDWithChapter batch)
+        //{
+        //    try
+        //    {
+        //        using (var package = new ExcelPackage(batch.File.OpenReadStream()))
+        //        {
+        //            var worksheet = package.Workbook.Worksheets[0]; // assuming the data is on the first worksheet
+        //            var rowCount = worksheet.Dimension.Rows;
+
+        //            // List of format strings to try when parsing the date
+        //            var dateFormats = new List<string>
+        //                   {
+        //                "dd-MM-yyyy HH:mm:ss",
+        //                "yyyy-MM-ddTHH:mm:ss",
+        //                "yyyy-MM-ddTHH:mm:ssZ",
+        //                "yyyy-MM-dd HH:mm:ss",
+        //                "yyyy/MM/dd HH:mm:ss",
+        //                "dd/MM/yyyy HH:mm:ss",
+        //                "dd-MMM-yyyy HH:mm:ss",
+        //                "dd MMM yyyy HH:mm:ss",
+        //                "MM/dd/yyyy HH:mm:ss",
+        //                "MMM dd, yyyy HH:mm:ss",
+        //                "dd-MM-yyyy",
+        //                "yyyy-MM-dd",
+        //                "yyyy/MM/dd",
+        //                "dd/MM/yyyy",
+        //                "dd-MMM-yyyy",
+        //                "dd MMM yyyy",
+        //                "MM/dd/yyyy",
+        //                "dd-MM-yyyy",
+        //                "MMM dd, yyyy"
+        //            };
+
+        //            for (int i = 2; i <= rowCount; i++) // assuming the first row contains headers
+        //            {
+        //                var chapterCode = worksheet.Cells[i, 1].Value?.ToString();
+        //                var chapterName = worksheet.Cells[i, 2].Value?.ToString();
+        //                var chapterDescription = worksheet.Cells[i, 3].Value?.ToString();
+        //                var expectedDateString = worksheet.Cells[i, 4].Value?.ToString();
+
+        //                DateTime expectedDate;
+        //                if (!DateTime.TryParseExact(expectedDateString, dateFormats.ToArray(), CultureInfo.InvariantCulture, DateTimeStyles.None, out expectedDate))
+        //                {
+        //                    throw new ArgumentException($"Invalid date format: {expectedDateString}");
+        //                }
+
+        //                var expectedDateFormatted = expectedDate.ToString("dd/MM/yyyy");
+        //                // create a new ChapterBinding object
+        //                var chapter = new ChatperBinding
+        //                {
+        //                    ChapterCode = chapterCode,
+        //                    ChapterName = chapterName,
+        //                    ChapterDescription = chapterDescription,
+        //                    ExpectedDate = DateTime.ParseExact(expectedDateFormatted, "dd/MM/yyyy", CultureInfo.InvariantCulture)
+        //                    //ExpectedDate = expectedDate.Date
+        //                };
+
+        //                // pass the chapter details along with the batch details to your service or database for processing
+        //                await _batchDetailService.CreateBatchDetail(batch, chapter);
+        //            }
+        //        }
+
+        //        // return a success response
+        //        return Ok();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        // return an error response
+        //        Log.Error(ex, "Error creating batch details.");
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
+
+
         [HttpPost]
         public async Task<ActionResult> CreateBatchDetail([FromForm] BDWithChapter batch)
         {
@@ -39,23 +160,52 @@ namespace LMS.Controllers
                     var worksheet = package.Workbook.Worksheets[0]; // assuming the data is on the first worksheet
                     var rowCount = worksheet.Dimension.Rows;
 
+                    // List of format strings to try when parsing the date
+                    var dateFormats = new List<string>
+{
+    "dd-MM-yyyy HH:mm:ss",
+    "yyyy-MM-ddTHH:mm:ss",
+    "yyyy-MM-ddTHH:mm:ssZ",
+    "yyyy-MM-dd HH:mm:ss",
+    "yyyy/MM/dd HH:mm:ss",
+    "dd/MM/yyyy HH:mm:ss",
+    "dd-MMM-yyyy HH:mm:ss",
+    "dd MMM yyyy HH:mm:ss",
+    "MM/dd/yyyy HH:mm:ss",
+    "MMM dd, yyyy HH:mm:ss",
+    "dd-MM-yyyy",
+    "yyyy-MM-dd",
+    "yyyy/MM/dd",
+    "dd/MM/yyyy",
+    "dd-MMM-yyyy",
+    "dd MMM yyyy",
+    "MM/dd/yyyy",
+    "MMM dd, yyyy"
+};
+
+
                     for (int i = 2; i <= rowCount; i++) // assuming the first row contains headers
                     {
                         var chapterCode = worksheet.Cells[i, 1].Value?.ToString();
                         var chapterName = worksheet.Cells[i, 2].Value?.ToString();
                         var chapterDescription = worksheet.Cells[i, 3].Value?.ToString();
                         var expectedDateString = worksheet.Cells[i, 4].Value?.ToString();
-                        var expectedDate = DateTime.ParseExact(expectedDateString, "dd-MM-yyyy HH:mm:ss", CultureInfo.InvariantCulture);
 
-                       // var expectedDate = DateTime.ParseExact(expectedDateString, "dd-MM-yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+                        DateTime expectedDate;
+                        if (!DateTime.TryParseExact(expectedDateString, dateFormats.ToArray(), CultureInfo.InvariantCulture, DateTimeStyles.None, out expectedDate))
+                        {
+                            throw new ArgumentException($"Invalid date format: {expectedDateString}");
+                        }
 
+                        var expectedDateFormatted = expectedDate.ToString("dd-MM-yyyy");
                         // create a new ChapterBinding object
                         var chapter = new ChatperBinding
                         {
                             ChapterCode = chapterCode,
                             ChapterName = chapterName,
                             ChapterDescription = chapterDescription,
-                            ExpectedDate = expectedDate.Date
+                            ExpectedDate = DateTime.ParseExact(expectedDateFormatted, "dd-MM-yyyy", CultureInfo.InvariantCulture)
+                            //ExpectedDate = expectedDate.Date
                         };
 
                         // pass the chapter details along with the batch details to your service or database for processing
@@ -73,7 +223,6 @@ namespace LMS.Controllers
                 return BadRequest(ex.Message);
             }
         }
-
 
 
 
